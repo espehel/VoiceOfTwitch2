42f1d7158891a2e168e39ab108eaf857375c28fe
espehel <espenhellerud5@gmail.com>
2014-12-22 19:02:22 +0100

    bot now listens to irc channels

--- config.js
+++ config.js
@@ -1,5 +1,9 @@
 var config = {
-	channels: [],
+	channels: ["#dotapit"],
 	server: "irc.twitch.tv",
-	botname: "Espen307"
+	botname: "Espen307",
+	port: "6667",
+	pass: "oauth:arzn22p9nyk8laep9t5jj8ccz42w3z4"
 };
+
+module.exports = config;
--- twitchlistener.js
+++ twitchlistener.js
@@ -1,4 +1,19 @@
 var irc = require("irc");
-var config = "require("./config");
+var config = require("./config");
 
+var bot = new irc.Client(config.server, config.botname, {
+	channels: config.channels,
+	port: config.port,
+	password: config.pass
+});
 
+console.log(config.server);
+
+bot.addListener('message', function(from, to, message){
+    console.log(from + ' => ' + to + ': ' + message);
+
+});
+
+bot.addListener('error', function(message) {
+    console.log('error: ', message);
+});
